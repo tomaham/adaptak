ea509254b2e662b661beff6085e5a098cd2812ba
tomas.hampejs <caerulus@gmail.com>
2024-09-08 10:40:23 +0200

    Update adaptak-mapa.py

--- adaptak-mapa.py
+++ adaptak-mapa.py
@@ -29,7 +29,6 @@ else:
 
 st.set_page_config(page_title="Gastromapa (Reli Adapťák 2024)", layout=layout)  # page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None
 
-st.write(layout)
 
 st.title("Adapťák Gastro Mapa")
 st.write("Kde se dá kolem najíst a napít?")
